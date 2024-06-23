ebb181f5e4d72915ef580508f0b76db2733c0ca0
BahaaAldein <BahaaAldein@gmail.com>
2024-06-23 12:40:56 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "utfs.io",
-        port: "",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
